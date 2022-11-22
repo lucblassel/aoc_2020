ffc33e19d82e8451fb4fab98b51fad020be8de58
Luc Blassel <luc.blassel@gmail.com>
2022-11-22 21:37:48 +0100

    days 7 and 8

---
+++ src/bin/07.rs
@@ -0,0 +1,73 @@
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    let input = include_str!("../../inputs/07.txt");
+
+    let parent_re = Regex::new(r"^(.+) bags contain").unwrap();
+    let child_re = Regex::new(r"(\d+) (\w+ \w+) bag").unwrap();
+
+    let mut containers: HashMap<&str, Vec<(&str, i32)>> = HashMap::new();
+    let mut containees: HashMap<&str, Vec<(&str, i32)>> = HashMap::new();
+
+    for line in input.lines() {
+        let parent = parent_re.captures(line).unwrap().get(1).unwrap().as_str();
+        for cap in child_re.captures_iter(line) {
+            let count = cap.get(1).unwrap().as_str().parse::<i32>().unwrap();
+            let color = cap.get(2).unwrap().as_str();
+
+            containers
+                .entry(parent)
+                .or_insert(vec![])
+                .push((color, count));
+            containees
+                .entry(color)
+                .or_insert(vec![])
+                .push((parent, count));
+        }
+    }
+
+    let mut to_visit: Vec<&str> = vec!["shiny gold"];
+    let mut visited: HashSet<&str> = HashSet::new();
+    while !to_visit.is_empty() {
+        match to_visit.pop() {
+            None => break,
+            Some(name) => {
+                containees
+                    .entry(name)
+                    .or_default()
+                    .iter()
+                    .map(|(name, _)| *name)
+                    .map(|name| {
+                        visited.insert(name);
+                        to_visit.push(name);
+                    })
+                    .count();
+            }
+        }
+    }
+
+    println!(
+        "{} bags can contain a shiny gold bag eventually",
+        visited.len()
+    );
+
+    println!(
+        "Shiny golden bags contain {} total bags",
+        count_contained("shiny gold", &containers) - 1
+    )
+}
+
+fn count_contained(name: &str, containers: &HashMap<&str, Vec<(&str, i32)>>) -> i32 {
+    let mut count = 1;
+
+    if let Some(contained) = containers.get(name) {
+        for (c_name, c_count) in contained {
+            count += (*c_count) * count_contained(c_name, containers)
+        }
+    } else {
+        return 1;
+    }
+
+    count
+}
---
+++ src/bin/08.rs
@@ -0,0 +1,101 @@
+use regex::Regex;
+use std::collections::HashSet;
+
+fn main() {
+    let input = include_str!("../../inputs/08.txt");
+    let regex = Regex::new(r"(nop|acc|jmp) (.+)").unwrap();
+
+    let instructions: Vec<(&str, i32)> = input
+        .lines()
+        .map(|line| {
+            regex
+                .captures_iter(line)
+                .map(|cap| {
+                    (
+                        cap.get(1).unwrap().as_str(),
+                        cap.get(2).unwrap().as_str().parse::<i32>().unwrap(),
+                    )
+                })
+                .next()
+                .unwrap()
+        })
+        .collect();
+
+    let (acc1, _) = execute_prog(&instructions);
+    println!("1) Acc: {acc1}");
+
+    for (cursor, instruction) in instructions.iter().enumerate() {
+        match instruction {
+            ("acc", _) => continue,
+            _ => {
+                let (acc, halted) = execute_with_switch(&instructions, cursor);
+                if halted {
+                    println!("2) Acc: {acc}");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn execute_prog(instructions: &Vec<(&str, i32)>) -> (i32, bool) {
+    let mut cursor: i32 = 0;
+    let mut acc = 0;
+    let mut executed: HashSet<i32> = HashSet::new();
+
+    while (cursor as usize) < instructions.len() {
+        if executed.contains(&cursor) {
+            return (acc, false);
+        }
+        executed.insert(cursor);
+
+        match instructions[cursor as usize] {
+            ("nop", _) => cursor += 1,
+            ("jmp", num) => cursor += num,
+            ("acc", num) => {
+                cursor += 1;
+                acc += num;
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    (acc, true)
+}
+
+fn execute_with_switch(instructions: &Vec<(&str, i32)>, switch_index: usize) -> (i32, bool) {
+    let mut cursor: i32 = 0;
+    let mut acc = 0;
+    let mut executed: HashSet<i32> = HashSet::new();
+
+    while (cursor as usize) < instructions.len() {
+        if executed.contains(&cursor) {
+            return (acc, false);
+        }
+        executed.insert(cursor);
+
+        match instructions[cursor as usize] {
+            ("nop", num) => {
+                if cursor as usize == switch_index {
+                    cursor += num;
+                } else {
+                    cursor += 1;
+                }
+            }
+            ("jmp", num) => {
+                if cursor as usize == switch_index {
+                    cursor += 1;
+                } else {
+                    cursor += num;
+                }
+            }
+            ("acc", num) => {
+                cursor += 1;
+                acc += num;
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    (acc, true)
+}
